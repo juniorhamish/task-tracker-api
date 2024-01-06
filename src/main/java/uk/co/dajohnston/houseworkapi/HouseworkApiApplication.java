66acd954c0edf714859440479bdd447b9bbfb07d
David Johnston <davidajohnston2000@gmail.com>
2024-01-06 14:48:36 +0000

    Initial commit

---
+++ src/main/java/uk/co/dajohnston/houseworkapi/HouseworkApiApplication.java
@@ -0,0 +1,13 @@
+package uk.co.dajohnston.houseworkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseworkApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HouseworkApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uk/co/dajohnston/houseworkapi/HouseworkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.dajohnston.houseworkapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseworkApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
