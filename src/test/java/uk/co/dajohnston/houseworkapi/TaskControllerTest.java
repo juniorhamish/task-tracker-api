c6371b1fc3276d45a2d16655ed6ebeb2b88398a1
David Johnston <davidajohnston2000@gmail.com>
2024-01-07 20:19:42 +0000

    Code coverage

--- src/test/java/uk/co/dajohnston/houseworkapi/TaskControllerTest.java
+++ src/test/java/uk/co/dajohnston/houseworkapi/TaskControllerTest.java
@@ -2,14 +2,31 @@ package uk.co.dajohnston.houseworkapi;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TaskControllerTest {
 
+  private TaskController controller;
+
+  @BeforeEach
+  void setUp() {
+    controller = new TaskController();
+  }
+
   @Test
   void tasks_shouldReturnTaskList() {
-    assertThat(new TaskController().tasks(), is(empty()));
+    controller.saveTask(new Task(1, "Task Name"));
+
+    assertThat(controller.tasks(), contains(new Task(1, "Task Name")));
+  }
+
+  @Test
+  void task_shouldReturnTaskWithGivenId() {
+    controller.saveTask(new Task(2, "My Task"));
+
+    assertThat(controller.task(2), is(new Task(2, "My Task")));
   }
 }
