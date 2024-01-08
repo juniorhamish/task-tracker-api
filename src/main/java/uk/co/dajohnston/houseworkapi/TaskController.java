916da46067e0d40871bf4a4788565e119ab2d7e6
David Johnston <davidajohnston2000@gmail.com>
2024-01-08 15:14:55 +0000

    Add logging

--- src/main/java/uk/co/dajohnston/houseworkapi/TaskController.java
+++ src/main/java/uk/co/dajohnston/houseworkapi/TaskController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,12 +12,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class TaskController {
 
   private static final Map<Long, Task> tasks = new HashMap<>();
 
   @GetMapping("/tasks")
   public List<Task> tasks() {
+    log.warn("Getting all tasks");
     return new ArrayList<>(tasks.values());
   }
 
