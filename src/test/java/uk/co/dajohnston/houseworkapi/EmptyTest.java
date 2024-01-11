7cf4e426ab7d7af8fcb772ab72b6b8f7260dfa3f
David Johnston <davidajohnston2000@gmail.com>
2024-01-11 00:23:00 +0000

    Make sure there is a test report

---
+++ src/test/java/uk/co/dajohnston/houseworkapi/EmptyTest.java
@@ -0,0 +1,15 @@
+package uk.co.dajohnston.houseworkapi;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class EmptyTest {
+
+  @Test
+  public void test() {
+    assertThat(1, is(1));
+  }
+
+}
