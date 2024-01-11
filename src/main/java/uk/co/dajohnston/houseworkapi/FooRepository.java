9b91ce6fadabc0d1ad9d33bc2e01e58791d11740
David Johnston <davidajohnston2000@gmail.com>
2024-01-11 00:19:14 +0000

    Basic Task entity

--- src/main/java/uk/co/dajohnston/houseworkapi/FooRepository.java
+++
@@ -1,7 +0,0 @@
-package uk.co.dajohnston.houseworkapi;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface FooRepository extends MongoRepository<Foo, String> {
-
-}
--- src/main/java/uk/co/dajohnston/houseworkapi/Task.java
+++ src/main/java/uk/co/dajohnston/houseworkapi/Task.java
@@ -2,8 +2,10 @@ package uk.co.dajohnston.houseworkapi;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
+@Document
 public class Task {
 
   @Id
