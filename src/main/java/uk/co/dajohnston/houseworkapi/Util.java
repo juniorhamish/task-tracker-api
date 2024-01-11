4d9cef5aa77e0eb8220733849510ed9a811b94d4
David Johnston <davidajohnston2000@gmail.com>
2024-01-11 01:45:13 +0000

    More code for coverage

--- src/main/java/uk/co/dajohnston/houseworkapi/Util.java
+++ src/main/java/uk/co/dajohnston/houseworkapi/Util.java
@@ -5,4 +5,20 @@ public class Util {
   public int add(int x, int y) {
     return x + y;
   }
+
+  public int subtract(int x, int y) {
+    return x - y;
+  }
+
+  public int multiply(int x, int y) {
+    return x * y;
+  }
+
+  public int divide(int x, int y) {
+    return x / y;
+  }
+
+  public int power(int x, int y) {
+    return x ^ y;
+  }
 }
