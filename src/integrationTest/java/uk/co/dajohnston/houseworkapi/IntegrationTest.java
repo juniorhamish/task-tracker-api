d9b2717a9fcc38af2c8ec229a96df3109ff635fd
David Johnston <davidajohnston2000@gmail.com>
2024-01-12 01:02:16 +0000

    Add proper test

--- src/integrationTest/java/uk/co/dajohnston/houseworkapi/IntegrationTest.java
+++ src/integrationTest/java/uk/co/dajohnston/houseworkapi/IntegrationTest.java
@@ -1,16 +1,23 @@
 package uk.co.dajohnston.houseworkapi;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class IntegrationTest {
 
+  @Autowired
+  private MockMvc mockMvc;
+
   @Test
-  void test() {
-    assertThat(5, is(5));
+  void test() throws Exception {
+    mockMvc.perform(get("/tasks")).andExpect(status().isOk());
   }
 }
