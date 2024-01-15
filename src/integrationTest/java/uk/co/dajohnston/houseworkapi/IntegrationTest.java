876ca5ca87c0fecec7f175fc95a451941ff217b8
David Johnston <davidajohnston2000@gmail.com>
2024-01-15 14:25:57 +0000

    Add test cleanup to remove DB after each test (#14)
    Also set a different database name for integration tests for when run
    locally (the value will be overridden by environment variables when run
    in CI/prod)

--- src/integrationTest/java/uk/co/dajohnston/houseworkapi/IntegrationTest.java
+++ src/integrationTest/java/uk/co/dajohnston/houseworkapi/IntegrationTest.java
@@ -4,10 +4,12 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
@@ -16,6 +18,13 @@ class IntegrationTest {
 
   @Autowired
   private MockMvc mockMvc;
+  @Autowired
+  private MongoTemplate mongoTemplate;
+
+  @AfterEach
+  void tearDown() {
+    mongoTemplate.getDb().drop();
+  }
 
   @Test
   void test() throws Exception {
