a8f40a88ede8095944837f83a402e44e0e715d76
David Johnston <davidajohnston2000@gmail.com>
2024-01-18 22:51:59 +0000

    Protect the API using JWT authentication (#15)
    Configured to validate the JWT tokens using Auth0 account.

---
+++ src/integrationTest/java/uk/co/dajohnston/houseworkapi/security/WithMockJWT.java
@@ -0,0 +1,12 @@
+package uk.co.dajohnston.houseworkapi.security;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import org.springframework.security.test.context.support.WithSecurityContext;
+
+@Retention(RetentionPolicy.RUNTIME)
+@WithSecurityContext(factory = WithMockJWTSecurityContextFactory.class)
+public @interface WithMockJWT {
+
+  String[] authorities() default {};
+}
---
+++ src/main/java/uk/co/dajohnston/houseworkapi/security/SecurityConfig.java
@@ -0,0 +1,27 @@
+package uk.co.dajohnston.houseworkapi.security;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+import static org.springframework.security.oauth2.core.authorization.OAuth2AuthorizationManagers.hasScope;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests(authorize -> authorize.requestMatchers("/actuator/**")
+                                                     .permitAll()
+                                                     .requestMatchers("/tasks")
+                                                     .access(hasScope("read:tasks"))
+                                                     .anyRequest()
+                                                     .authenticated())
+        .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()));
+    return http.build();
+  }
+}
