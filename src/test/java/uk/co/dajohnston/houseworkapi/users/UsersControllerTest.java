01f3efbc9938b297dc459f6771959331fb6a5ef9
David Johnston <davidajohnston2000@gmail.com>
2024-01-24 22:26:51 +0000

    Test with security enabled

--- src/test/java/uk/co/dajohnston/houseworkapi/users/UsersControllerTest.java
+++ src/test/java/uk/co/dajohnston/houseworkapi/users/UsersControllerTest.java
@@ -15,12 +15,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
+import uk.co.dajohnston.houseworkapi.security.SecurityConfig;
 import uk.co.dajohnston.houseworkapi.security.WithMockJWT;
 
 @WebMvcTest(UsersController.class)
 @AutoConfigureMockMvc
-@WithMockJWT
+@Import(SecurityConfig.class)
+@WithMockJWT(authorities = {"SCOPE_read:users", "SCOPE_create:users"})
 class UsersControllerTest {
 
   @Autowired
