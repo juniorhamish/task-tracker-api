e51f8adb0751c815c3b23dd540dfb378568f2c80
David Johnston <davidajohnston2000@gmail.com>
2024-08-01 23:29:55 +0100

    Test IT DB

--- src/integrationTest/java/uk/co/dajohnston/houseworkapi/users/UsersTest.java
+++ src/integrationTest/java/uk/co/dajohnston/houseworkapi/users/UsersTest.java
@@ -7,12 +7,10 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.web.servlet.MockMvc;
 import uk.co.dajohnston.houseworkapi.security.WithMockJWT;
 
@@ -22,13 +20,6 @@ class UsersTest {
 
   @Autowired
   private MockMvc mockMvc;
-  @Autowired
-  private MongoTemplate mongoTemplate;
-
-  @AfterEach
-  void tearDown() {
-    mongoTemplate.getDb().drop();
-  }
 
   @Test
   @WithMockJWT(scope = "create:users read:users", roles = "Admin")
