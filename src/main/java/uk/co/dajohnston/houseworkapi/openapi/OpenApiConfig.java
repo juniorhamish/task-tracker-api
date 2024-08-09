04997d630d31c4db933fb377d0e74ce69a9864da
David Johnston <davidajohnston2000@gmail.com>
2024-08-09 20:58:30 +0100

    Add Swagger documentation

---
+++ src/main/java/uk/co/dajohnston/houseworkapi/openapi/OpenApiConfig.java
@@ -0,0 +1,29 @@
+package uk.co.dajohnston.houseworkapi.openapi;
+
+import static io.swagger.v3.oas.annotations.enums.SecuritySchemeType.OAUTH2;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.security.OAuthFlow;
+import io.swagger.v3.oas.annotations.security.OAuthFlows;
+import io.swagger.v3.oas.annotations.security.OAuthScope;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(security = {@SecurityRequirement(name = "OAuth2")})
+@SecurityScheme(
+    name = "OAuth2",
+    type = OAUTH2,
+    flows =
+        @OAuthFlows(
+            password =
+                @OAuthFlow(
+                    scopes = {
+                      @OAuthScope(name = "create:users"),
+                      @OAuthScope(name = "read:users"),
+                      @OAuthScope(name = "offline_access")
+                    },
+                    tokenUrl = "https://dajohnston.eu.auth0.com/oauth/token",
+                    authorizationUrl = "https://dajohnston.eu.auth0.com/oauth/authorize")))
+public class OpenApiConfig {}
